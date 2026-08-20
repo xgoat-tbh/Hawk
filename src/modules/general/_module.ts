7469b413f32a43d7cbce46d00fbaf726b5bf8b91
xgoat-tbh <xgoat-tbh@users.noreply.github.com>
2026-08-20 09:55:03 +0530

    refactor(general): remove avatar, banner, userinfo, and serverinfo commands

--- src/modules/general/_module.ts
+++ src/modules/general/_module.ts
@@ -8,7 +8,7 @@ import { handleAfkMessage } from './_afkHandler.js';
 
 export default {
   name: 'general',
-  description: 'General utility commands (help, ping, info, AFK, steal, ignore, avatar, banner, userinfo, serverinfo)',
+  description: 'General utility commands (help, ping, info, AFK, steal, ignore)',
   buttonPrefixes: ['ping_refresh_', 'info_', 'steal_btn_', 'help_page_'],
   selectPrefixes: ['help_'],
   modalPrefixes: ['steal_modal_'],
